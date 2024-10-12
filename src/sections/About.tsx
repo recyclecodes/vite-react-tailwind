e0d74e7670c08d89014083ffc7ae177748e72f1b
Godfrey Anthony T. Serojales <gaserojales@gmail.com>
2024-10-12 11:36:56 +0800

    added: rb2b for b2b

--- src/sections/About.tsx
+++ src/sections/About.tsx
@@ -1,7 +1,7 @@
-import { pieChart } from '../assets/image';
-import AOS from 'aos';
-import 'aos/dist/aos.css';
-import { useEffect } from 'react';
+import { pieChart } from "../assets/image";
+import AOS from "aos";
+import "aos/dist/aos.css";
+import { useEffect } from "react";
 
 const About = () => {
   useEffect(() => {
@@ -10,32 +10,30 @@ const About = () => {
   return (
     <section
       id="about"
-      className="flex justify-between items-center max-xl:flex-col-reverse gap-10 lg:w-[65%] mx-auto h-full"
+      className="mx-auto flex h-full items-center justify-between gap-10 max-xl:flex-col-reverse lg:w-[65%]"
     >
-      <div data-aos='fade-right'
-        className="flex-1"
-      >
+      <div data-aos="fade-right" className="flex-1">
         <img
           src={pieChart}
           alt="Pie Chart"
           width={610}
           height={578}
-          className="object-contain w-full"
+          className="w-full object-contain"
         />
       </div>
-      <div data-aos='fade-left' className="flex flex-1 flex-col">
+      <div data-aos="fade-left" className="flex flex-1 flex-col">
         <div className="relative mb-8">
-          <div className="absolute w-16 h-12 left-[170px] top-[-4px] bg-accent"></div>
-          <h2 className="text-4xl lg:text-5xl font-bold ">About Me.</h2>
+          <div className="absolute left-[170px] top-[-4px] h-12 w-16 bg-accent"></div>
+          <h2 className="text-4xl font-bold lg:text-5xl ">About Me.</h2>
         </div>
         <span>
           <p className="mt-4">
-            Hello! I'm{' '}
+            Hello! I'm{" "}
             <span className="font-semibold">Godfrey Anthony Serojales,</span> a
-            full-stack developer driven by{' '}
+            full-stack developer driven by{" "}
             <span className="font-semibold">
-              {' '}
-              creativity and problem-solving{' '}
+              {" "}
+              creativity and problem-solving{" "}
             </span>
             . My tech journey began with a passion for design, photography, and
             the intricate world of Gunpla kits, leading me to excel in crafting
@@ -43,12 +41,12 @@ const About = () => {
           </p>
           <p className="mt-4 text-justify">
             When not coding, you'll find me capturing moments through my lens or
-            meticulously assembling Gunpla models, where{' '}
+            meticulously assembling Gunpla models, where{" "}
             <span className="font-semibold">attention to detail</span> reigns
             supreme. I also thrive on the challenge of logic games, relishing
             the thrill of cracking complex puzzles. With a diverse range of
             interests, I bring a unique perspective to every project, blending
-            creativity with technical expertise to deliver{' '}
+            creativity with technical expertise to deliver{" "}
             <span className="font-semibold"> flawless solutions</span> that
             delight the senses. Let's collaborate and create something
             extraordinary!
